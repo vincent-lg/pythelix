6d35622979305e96c8e199776eb751839b9ed4a2
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2025-05-31 09:13:40 +0200

    Bump to 0.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pythelix.MixProject do
   def project do
     [
       app: :pythelix,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
